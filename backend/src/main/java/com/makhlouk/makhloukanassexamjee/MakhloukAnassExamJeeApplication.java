7579c620ad06fc0e28fc568a7829aa5c47ded552
vnvss-0x <anassmakhlo@gmail.com>
2026-05-11 15:37:20 +0100

    Initial commit : Structure backend et frontend

---
+++ backend/src/main/java/com/makhlouk/makhloukanassexamjee/MakhloukAnassExamJeeApplication.java
@@ -0,0 +1,13 @@
+package com.makhlouk.makhloukanassexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakhloukAnassExamJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakhloukAnassExamJeeApplication.class, args);
+	}
+
+}
